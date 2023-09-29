a52a08b1364f05e851ab1ca5c9c47689c0ad4134
GarrickC23 <garrickchiu0723@gmail.com>
2023-09-29 14:46:23 -0700

    Playing with OpenGL

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,25 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <vector>
+#include <cstdlib>
 
 using namespace std; 
+
+struct Particle {
+    float x, y;  // Position
+    float vy;    // Vertical Velocity
+};
+
+Particle particle;
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, width, height);
 }
+
+const int WINDOW_WIDTH = 1000;
+const int WINDOW_HEIGHT = 800;
 
 int main()
 {
@@ -23,7 +35,7 @@ int main()
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     GLFWwindow* window;
-    window = glfwCreateWindow(800, 600, "ZMMR", NULL, NULL);
+    window = glfwCreateWindow(800, 600, "OpenGL", NULL, NULL);
     if (window == NULL)
     {
         cout << "Failed to open GLFW window" << endl;
@@ -37,11 +49,27 @@ int main()
         return -1;
     }
 
+    particle.x = WINDOW_WIDTH / 2.0f;
+    particle.y = WINDOW_HEIGHT / 2.0f;
+    particle.vy = 2.0f;  // Moving upward
+
     glViewport(0, 0, 800, 600);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     while(!glfwWindowShouldClose(window))
     {
+        // Clear screen
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Update the particle position
+        particle.y += particle.vy;
+
+        // Draw the particle
+        glBegin(GL_POINTS);
+        glVertex2f(particle.x / (WINDOW_WIDTH / 2.0f) - 1.0f, particle.y / (WINDOW_HEIGHT / 2.0f) - 1.0f);
+        glEnd();
+        
         glfwSwapBuffers(window);
         glfwPollEvents();    
     }
